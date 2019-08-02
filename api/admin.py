6c78440b30f31c1dae21a5385d43ca42120898c4
dwcope3 <dwcope3@gmail.com>
2019-08-01 22:21:24 -0400

    make models

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from api.models import StarterDeck, Card
 
 # Register your models here.
+admin.site.register(StarterDeck)
+admin.site.register(Card)
--- api/models.py
+++ api/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class StarterDeck (models.Model):
+    title = models.CharField(max_length=100)
+    deck_image = models.URLField(max_length=250)
+    
+    def __str__(self):
+        return self.title
+
+
+class Card (models.Model):
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=250)
+    deck = models.ForeignKey(to=StarterDeck, on_delete=models.CASCADE)
+    card_image = models.URLField(max_length=250)
+
+    def __str__(self):
+        return self.title
