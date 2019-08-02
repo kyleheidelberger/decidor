e166db0d2b2818032ebc176ea50cda4244d0c9e2
dwcope3 <dwcope3@gmail.com>
2019-08-02 15:07:32 -0400

    add SITE_ID = 1 to settings.py

--- decider/settings.py
+++ decider/settings.py
@@ -47,6 +47,8 @@ INSTALLED_APPS = [
     'corsheaders',  
 ]
 
+SITE_ID = 1
+
 LOGIN_REDIRECT_URL = ''
 ACCOUNT_ACTIVATION_DAYS = 7 
 # One-week activation window; you may, of course, use a different value.
