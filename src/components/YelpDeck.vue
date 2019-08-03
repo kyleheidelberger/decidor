69620c498ea61abc9934227721cf59fa807c7c91
Kyle Heidelberger <kyle.heidelberger@gmail.com>
2019-08-03 18:33:03 -0400

    add prompt for location when user first clicks deck

--- src/components/YelpDeck.vue
+++ src/components/YelpDeck.vue
@@ -1,5 +1,3 @@
-
-
 <template>
   <section class="buttons-container2" id="choice-logic2">
     <button
@@ -12,9 +10,12 @@
     >{{option}}</button>
 
     <div class="searchBar">
-      <input type="text" v-model.lazy="cityName" placeholder="City" />
+      <input type="text" v-model.lazy="cityName" v-on:change="getBusinesses" placeholder="City" />
       <button v-on:click="getBusinesses">Get Businesses</button>
-      <p>Input your current location. You are in: {{ cityName }}</p>
+      <p>
+        Input your current location.
+        You are currently looking in: {{ cityName }}
+      </p>
     </div>
 
     <div v-for="business in businesses" :key="`${business}`" class="yelp-business">
@@ -56,27 +57,12 @@ export default {
     };
   },
   mounted() {
-    this.getCityName();
-    // console.log(this.cityName);
-    // this.getBusinesses(this.cityName);
-    // axios
-    //   .get(
-    //     `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?categories=food,all&cityName=${this.cityName}`,
-    //     {
-    //       headers: {
-    //         "Access-Control-Allow-Origin": "*",
-    //         Authorization: `Bearer SM6pJx7LlCeKgElTpKU3PgsBDvqZud92PBBhqRBOEunqL9az6MmnAN9GUf4_mjjQva10STsyAlOs6RacEdskjV3qx7X_SjhqtpFVY0G0KzKvDoXcb4s-X2eZHOc5XXYx`
-    //       }
-    //     }
-    //   )
-    //   .then(response => {
-    //     this.businesses = response.data.businesses;
-    //   });
+    //   this.getCityName();
+    //   // console.log(this.cityName);
+    this.cityName = prompt("What city are you in?");
+    this.getBusinesses(this.cityName);
   },
   methods: {
-    getCityName() {
-      console.log(this.cityName);
-    },
     getBusinesses(cityName) {
       let apiURL = buildURL(this.cityName);
       axios
@@ -93,6 +79,6 @@ export default {
           console.log(error);
         });
     }
-  },
+  }
 };
 </script>
