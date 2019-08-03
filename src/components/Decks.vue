3f8805290c2385645cb19efb56a501dd51d1fab8
Kyle Heidelberger <kyle.heidelberger@gmail.com>
2019-08-03 10:58:38 -0400

    created vue for geolocation (not working) and yelpdeck (using copied logic)

--- src/components/Decks.vue
+++ src/components/Decks.vue
@@ -1,39 +1,67 @@
 <template>
-  <section class='deck'>
-    <div class='container'>
-        <h2 
+  <section class="deck">
+    <div class="container">
+      <h2
         v-if="hiddenCards"
-        @click='hiddenCards = false, hiddenDeck = true'
-        class='netflixTitle'>Netflix
-        </h2>
-            <button 
-            class='deckButton'
-            @click='hiddenCards = false, hiddenDeck = true'
-            :class='{ hiddenDick: hiddenDeck }'>
-            <img class="netflixLogo" src='https://cdn.vox-cdn.com/thumbor/AwKSiDyDnwy_qoVdLPyoRPUPo00=/39x0:3111x2048/1400x1400/filters:focal(39x0:3111x2048):format(png)/cdn.vox-cdn.com/uploads/chorus_image/image/49901753/netflixlogo.0.0.png'>
-            </button>
-    </div>
-    
-    <div v-if='!hiddenCards'>
-        <ChoiceLogic />
+        @click="hiddenCards = false, hiddenDeck = true"
+        class="netflixTitle"
+      >Netflix</h2>
+      <button
+        class="deckButton"
+        @click="hiddenCards = false, hiddenDeck = true"
+        :class="{ hiddenDick: hiddenDeck }"
+      >
+        <img
+          class="netflixLogo"
+          src="https://cdn.vox-cdn.com/thumbor/AwKSiDyDnwy_qoVdLPyoRPUPo00=/39x0:3111x2048/1400x1400/filters:focal(39x0:3111x2048):format(png)/cdn.vox-cdn.com/uploads/chorus_image/image/49901753/netflixlogo.0.0.png"
+        />
+      </button>
     </div>
 
+    <div class="container">
+      <h2
+        v-if="hiddenCards2"
+        @click="hiddenCards2 = false, hiddenDeck = true"
+        class="yelpDeckTitle"
+      >Yelp</h2>
+      <button
+        class="yelpDeckButton"
+        @click="hiddenCards2 = false, hiddenDeck = true"
+        :class="{ hiddenDick: hiddenDeck }"
+      >
+        <img
+          class="yelpLogo"
+          src="https://blog.yelp.com/wp-content/themes/yelpblog-updated/images/yelp-avatar.png"
+        />
+      </button>
+    </div>
+
+    <div v-if="!hiddenCards">
+      <ChoiceLogic />
+    </div>
+
+    <div v-if="!hiddenCards2">
+      <YelpDeck />
+    </div>
   </section>
 </template>
 
 <script>
-import ChoiceLogic from './ChoiceLogic'
+import ChoiceLogic from "./ChoiceLogic";
+import YelpDeck from "./YelpDeck";
 
 export default {
-  name: 'Decks',
+  name: "Decks",
   components: {
-    ChoiceLogic
+    ChoiceLogic,
+    YelpDeck
   },
   data: () => {
     return {
       hiddenCards: true,
+      hiddenCards2: true,
       hiddenDeck: false
-    }
+    };
   }
-}
+};
 </script>
---
+++ src/components/YelpDeck.vue
@@ -0,0 +1,76 @@
+<template>
+  <section class='buttons-container' id='choice-logic'>
+    <button
+      v-for='(option, index) in options'
+      :key='`${option}${index}`'
+      @click='selectOption(index)'
+      class='option-buttons button-style'
+      :disabled='validated === true'
+      :class='{ finalChoice: onlyChoice }'
+    >{{option}}</button>
+  </section>
+</template>
+
+
+<script>
+export default {
+  name: 'ChoiceLogic',
+  props: {
+    choices: {
+      type: Array,
+      default() {
+        return ['Queer Eye', 'Stranger Things', 
+        'Bojack Horseman', 'Orange is the New Black', 
+        'Unbreakable Kimmy Schmidt', 'American Vandal', 
+        'Mindhunter', 'Russian Doll',
+        'Master of None', 'GLOW',
+        'The Umbrella Academy', 'Big Mouth', 
+        'Dark', 'The Crown']
+      }
+    }
+  },
+  data: () => {
+    return {
+      onlyChoice: false,
+      validated: false,
+      options: [],
+      currentIndex: 0,
+      preferences: [],
+      copyChoiceList: [],
+      endIndex: 2
+    };
+  },
+  mounted() {
+    this.copyChoiceList = this.choices;
+    this.setOptions();
+  },
+  methods: {
+    setOptions() {
+      this.endIndex = this.currentIndex + 2;
+      console.log('pref:', this.preferences)
+      this.options = this.copyChoiceList.slice(this.currentIndex, this.endIndex);
+      console.log('before:', this.endIndex)
+    },
+    selectOption(index) {
+      this.currentIndex = this.currentIndex + 2;
+      const toAdd = this.options[index];
+      this.preferences.push(toAdd);
+      console.log('copychoicelist:', this.copyChoiceList)
+      if (this.endIndex >= this.copyChoiceList.length - 1) {
+        if (this.endIndex != this.copyChoiceList.length) {
+          this.preferences.push(this.copyChoiceList[this.copyChoiceList.length - 1])
+        }
+          this.currentIndex = 0;
+          this.copyChoiceList = this.preferences;
+          this.preferences = [];
+          console.log('after:', this.endIndex)
+      }
+      if (this.copyChoiceList.length === 1) {
+          this.onlyChoice = true;
+          this.validated = true;
+      }
+      this.setOptions();
+    },
+  }
+};
+</script>
