b82dcaeae52f19f9ac4e8264a7228e1f01330414
Kyle Heidelberger <kyle.heidelberger@gmail.com>
2019-08-03 11:41:34 -0400

    got JSON data from Yelp API in Vue

--- src/components/YelpDeck.vue
+++ src/components/YelpDeck.vue
@@ -1,31 +1,46 @@
+
+
 <template>
-  <section class='buttons-container' id='choice-logic'>
+  <section class="buttons-container" id="choice-logic">
     <button
-      v-for='(option, index) in options'
-      :key='`${option}${index}`'
-      @click='selectOption(index)'
-      class='option-buttons button-style'
-      :disabled='validated === true'
-      :class='{ finalChoice: onlyChoice }'
+      v-for="(option, index) in options"
+      :key="`${option}${index}`"
+      @click="selectOption(index)"
+      class="option-buttons button-style"
+      :disabled="validated === true"
+      :class="{ finalChoice: onlyChoice }"
     >{{option}}</button>
+
+    <div id="YelpDeck">{{ info }}</div>
   </section>
 </template>
 
 
 <script>
+import axios from "axios";
+
 export default {
-  name: 'ChoiceLogic',
+  name: "YelpDeck",
   props: {
     choices: {
       type: Array,
       default() {
-        return ['Queer Eye', 'Stranger Things', 
-        'Bojack Horseman', 'Orange is the New Black', 
-        'Unbreakable Kimmy Schmidt', 'American Vandal', 
-        'Mindhunter', 'Russian Doll',
-        'Master of None', 'GLOW',
-        'The Umbrella Academy', 'Big Mouth', 
-        'Dark', 'The Crown']
+        return [
+          "Queer Eye",
+          "Stranger Things",
+          "Bojack Horseman",
+          "Orange is the New Black",
+          "Unbreakable Kimmy Schmidt",
+          "American Vandal",
+          "Mindhunter",
+          "Russian Doll",
+          "Master of None",
+          "GLOW",
+          "The Umbrella Academy",
+          "Big Mouth",
+          "Dark",
+          "The Crown"
+        ];
       }
     }
   },
@@ -37,40 +52,52 @@ export default {
       currentIndex: 0,
       preferences: [],
       copyChoiceList: [],
-      endIndex: 2
+      endIndex: 2,
+      info: null
     };
   },
   mounted() {
+    axios.get(
+      `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?categories=food,all&location=Durham`,
+    {headers: {
+          "Access-Control-Allow-Origin": "*",
+          Authorization: `Bearer SM6pJx7LlCeKgElTpKU3PgsBDvqZud92PBBhqRBOEunqL9az6MmnAN9GUf4_mjjQva10STsyAlOs6RacEdskjV3qx7X_SjhqtpFVY0G0KzKvDoXcb4s-X2eZHOc5XXYx`
+        }}).then(response => (this.info = response));
     this.copyChoiceList = this.choices;
     this.setOptions();
   },
   methods: {
     setOptions() {
       this.endIndex = this.currentIndex + 2;
-      console.log('pref:', this.preferences)
-      this.options = this.copyChoiceList.slice(this.currentIndex, this.endIndex);
-      console.log('before:', this.endIndex)
+      console.log("pref:", this.preferences);
+      this.options = this.copyChoiceList.slice(
+        this.currentIndex,
+        this.endIndex
+      );
+      console.log("before:", this.endIndex);
     },
     selectOption(index) {
       this.currentIndex = this.currentIndex + 2;
       const toAdd = this.options[index];
       this.preferences.push(toAdd);
-      console.log('copychoicelist:', this.copyChoiceList)
+      console.log("copychoicelist:", this.copyChoiceList);
       if (this.endIndex >= this.copyChoiceList.length - 1) {
         if (this.endIndex != this.copyChoiceList.length) {
-          this.preferences.push(this.copyChoiceList[this.copyChoiceList.length - 1])
+          this.preferences.push(
+            this.copyChoiceList[this.copyChoiceList.length - 1]
+          );
         }
-          this.currentIndex = 0;
-          this.copyChoiceList = this.preferences;
-          this.preferences = [];
-          console.log('after:', this.endIndex)
+        this.currentIndex = 0;
+        this.copyChoiceList = this.preferences;
+        this.preferences = [];
+        console.log("after:", this.endIndex);
       }
       if (this.copyChoiceList.length === 1) {
-          this.onlyChoice = true;
-          this.validated = true;
+        this.onlyChoice = true;
+        this.validated = true;
       }
       this.setOptions();
-    },
+    }
   }
 };
 </script>
