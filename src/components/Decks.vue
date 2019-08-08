1a23bb5f1194684b0f74fb9384181b476ec4622b
Kyle Heidelberger <kyle.heidelberger@gmail.com>
2019-08-07 20:15:19 -0400

    nyt fiction and nonfiction decks

--- src/components/Decks.vue
+++ src/components/Decks.vue
@@ -1,22 +1,22 @@
 <template>
   <section class="deck">
-  <h1 v-if="!hiddenNav" class='decks-info'>Your decks</h1>
-  
-      <div v-if="!hiddenSearch" class="searchBar">
-        <input type="text" v-model.lazy="cityName" v-on:change="getBusinesses" placeholder="City" />
-        <button @click="getBusinesses">Get Businesses</button>
-        <p>
-          Input your current location.
-          You are currently looking in: {{ cityName }}
-        </p>
-      </div>
+    <h1 v-if="!hiddenNav" class="decks-info">Your decks</h1>
 
-      <div v-if="!hiddenNetflix">
-        <ChoiceLogic :choices="allDecks.netflixDeck" />
-      </div>
+    <div v-if="!hiddenSearch" class="searchBar">
+      <input type="text" v-model.lazy="cityName" v-on:change="getBusinesses" placeholder="City" />
+      <button @click="getBusinesses">Get Businesses</button>
+      <p>
+        Input your current location.
+        You are currently looking in: {{ cityName }}
+      </p>
+    </div>
 
-      <div v-if="!hiddenFood">
-        <ChoiceLogic :choices="allDecks.fastFoodDeck" />
+    <div v-if="!hiddenNetflix">
+      <ChoiceLogic :choices="allDecks.netflixDeck" />
+    </div>
+
+    <div v-if="!hiddenFood">
+      <ChoiceLogic :choices="allDecks.fastFoodDeck" />
     </div>
 
     <div v-if="!hiddenActivity">
@@ -31,63 +31,65 @@
       <ChoiceLogic :choices="yelpDecks.yelpRestaurants" />
     </div>
 
-    <div v-if="!hiddenBooks">
-      <ChoiceLogic :choices="apiDecks.bookDeck" />
+    <div v-if="!hiddenFictionBooks">
+      <ChoiceLogic :choices="apiDecks.fictionDeck" />
     </div>
 
+    <div v-if="!hiddenNonFictionBooks">
+      <ChoiceLogic :choices="apiDecks.nonFictionDeck" />
     </div>
 
-  <transition appear
-  name="bounce">
-    <div class="container">
-
-    <h2 v-if="!hiddenNav" class='deckInfo'>Starter Decks</h2>
-      <button
-        id="deckButton"
-        v-for="(deck, key) in allDecks"
-        :key="`${deck}${key}`"
-        class="deckButton"
-        @click="sendKey(key)"
-        :class="{ hiddenDick: hiddenDeck }">
-          <div class='deck-container'>
-            <h2 class='deckTitle'>{{deck.title}}</h2>
-              <div class='overlay'>
-                <img class="deckImage" :src="deck.image" />
-              </div>
-          </div>
-      </button>
-
-    <h2 v-if="!hiddenNav" class='deckInfo2'>Local Decks</h2>
-      <button
-      v-for="(deck, key) in yelpDecks"
-      :key="`${deck}${key}`"
-      class="deckButton"
-      @click="sendKey(key)"
-      :class="{ hiddenDick: hiddenDeck }">
-        <div class='deck-container'>
-          <h2 class='deckTitle'>{{deck.title}}</h2>
-            <div class='overlay'>
+    <transition appear name="bounce">
+      <div class="container">
+        <h2 v-if="!hiddenNav" class="deckInfo">Starter Decks</h2>
+        <button
+          id="deckButton"
+          v-for="(deck, key) in allDecks"
+          :key="`${deck}${key}`"
+          class="deckButton"
+          @click="sendKey(key)"
+          :class="{ hiddenDick: hiddenDeck }"
+        >
+          <div class="deck-container">
+            <h2 class="deckTitle">{{deck.title}}</h2>
+            <div class="overlay">
               <img class="deckImage" :src="deck.image" />
             </div>
-      </div>
-      </button>
-
-    <h2 v-if="!hiddenNav" class='deckInfo3'>API Decks</h2>
-      <button
-        v-for="(deck, key) in apiDecks"
-        :key="`${deck}${key}`"
-        class="deckButton"
-        @click="sendKey(key)"
-        :class="{ hiddenDick: hiddenDeck }">
-          <div class='deck-container'>
-            <h2 class='deckTitle'>{{deck.title}}</h2>
-              <div class='overlay'>
-                <img class="deckImage" :src="deck.image" />
-              </div>
           </div>
-      </button>
+        </button>
 
-    </div>
+        <h2 v-if="!hiddenNav" class="deckInfo2">Local Decks</h2>
+        <button
+          v-for="(deck, key) in yelpDecks"
+          :key="`${deck}${key}`"
+          class="deckButton"
+          @click="sendKey(key)"
+          :class="{ hiddenDick: hiddenDeck }"
+        >
+          <div class="deck-container">
+            <h2 class="deckTitle">{{deck.title}}</h2>
+            <div class="overlay">
+              <img class="deckImage" :src="deck.image" />
+            </div>
+          </div>
+        </button>
+
+        <h2 v-if="!hiddenNav" class="deckInfo3">API Decks</h2>
+        <button
+          v-for="(deck, key) in apiDecks"
+          :key="`${deck}${key}`"
+          class="deckButton"
+          @click="sendKey(key)"
+          :class="{ hiddenDick: hiddenDeck }"
+        >
+          <div class="deck-container">
+            <h2 class="deckTitle">{{deck.title}}</h2>
+            <div class="overlay">
+              <img class="deckImage" :src="deck.image" />
+            </div>
+          </div>
+        </button>
+      </div>
     </transition>
   </section>
 </template>
@@ -101,10 +103,11 @@ import { callbackify } from "util";
 
 const yelpBaseURL =
   "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?";
-const apiKey =
+const yelpApiKey =
   "SM6pJx7LlCeKgElTpKU3PgsBDvqZud92PBBhqRBOEunqL9az6MmnAN9GUf4_mjjQva10STsyAlOs6RacEdskjV3qx7X_SjhqtpFVY0G0KzKvDoXcb4s-X2eZHOc5XXYx";
 
-const bookBaseURL = "https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key=4QC7YMXjnIWo1dTtGFpj5itZlVDPvbOk";
+const bookBaseURL = `https://api.nytimes.com/svc/books/v3/lists/current/`;
+const nytApiKey = `4QC7YMXjnIWo1dTtGFpj5itZlVDPvbOk`;
 
 export default {
   name: "Decks",
@@ -126,11 +129,13 @@ export default {
         yelpParks: []
       },
       apiDecks: {
-        bookDeck: []
+        fictionDeck: [],
+        nonFictionDeck: []
       },
       // hiddenCards: true,
       cityName: "",
       searchingFor: "",
+      book_category: "",
       hiddenDeck: false,
       businesses: [],
       results: [],
@@ -139,7 +144,8 @@ export default {
       hiddenFood: true,
       hiddenActivity: true,
       hiddenFoodTypes: true,
-      hiddenBooks: true,
+      hiddenFictionBooks: true,
+      hiddenNonFictionBooks: true,
       hiddenSearch: true,
       hiddenNav: false
     };
@@ -157,7 +163,7 @@ export default {
     // this.allDecks[key] = await axios.get
     // }
     // });
-  
+
     this.allDecks.netflixDeck = [
       {
         title: "Queer Eye",
@@ -576,13 +582,18 @@ export default {
     this.yelpDecks.yelpParks.image =
       "https://www.discoverdurham.com/imager/s3_us-east-1_amazonaws_com/durham-2019/images/Nature-Science/DUKE_GARDENS_BRIDGE_aea8419375870281fb13854150585c99.jpg";
     this.yelpDecks.yelpParks.title = "Parks";
-    this.apiDecks.bookDeck.image = "https://decidor.s3.amazonaws.com/books.jpeg";
-    this.apiDecks.bookDeck.title = "NYT Books";
+    this.apiDecks.fictionDeck.image =
+      "https://decidor.s3.amazonaws.com/books.jpeg";
+    this.apiDecks.fictionDeck.title = "NYT Fiction";
+    this.apiDecks.nonFictionDeck.image =
+      "https://decidor.s3.amazonaws.com/books.jpeg";
+    this.apiDecks.nonFictionDeck.title = "NYT Non-Fiction";
   },
   methods: {
     sendKey(key) {
       this.getCardsOnClick(key);
       this.getSearchParam(key);
+      this.getBookCategory(key);
     },
     getCardsOnClick(key) {
       this.hiddenDeck = true;
@@ -595,9 +606,7 @@ export default {
         this.hiddenActivity = false;
       } else if (key.includes("foodTypesDeck")) {
         this.hiddenFoodTypes = false;
-      } else if (key.includes("bookDeck")) {
-        this.getBookList();
-      } else {
+      } else if (key.includes("yelp")) {
         this.hiddenSearch = false;
       }
     },
@@ -612,22 +621,26 @@ export default {
         this.searchingFor = "categories=parks,dog_parks,skate_parks";
       }
     },
-    buildURL(url, searchingFor, location) {
+    buildYelpURL(url, searchingFor, location) {
       return `${yelpBaseURL}${this.searchingFor}&location=${this.cityName}`;
     },
     getBusinesses(key) {
-      let apiURL = this.buildURL(yelpBaseURL, this.searchingFor, this.cityName);
+      let apiURL = this.buildYelpURL(
+        yelpBaseURL,
+        this.searchingFor,
+        this.cityName
+      );
 
       axios
         .get(apiURL, {
           headers: {
             "Access-Control-Allow-Origin": "*",
-            Authorization: `Bearer ${apiKey}`
+            Authorization: `Bearer ${yelpApiKey}`
           }
         })
         .then(response => {
           this.yelpDecks.businesses = response.data.businesses;
-          console.log("yelpresults:", this.yelpDecks.businesses)
+          console.log("yelpresults:", this.yelpDecks.businesses);
           this.filterBusinesses(this.yelpDecks.businesses);
           this.hiddenBusiness = false;
         })
@@ -653,35 +666,91 @@ export default {
         return this.yelpDecks.yelpRestaurants;
       });
     },
-    getBookList(key) {
-     axios
-        .get(bookBaseURL)
+    getBookCategory(key) {
+      if (key.includes("fictionDeck")) {
+        this.book_category = "hardcover-fiction";
+        this.getFictionBookList(key);
+      }
+      if (key.includes("nonFictionDeck")) {
+        this.book_category = "hardcover-nonfiction";
+        this.getNonFictionBookList(key);
+      }
+    },
+    buildBookURL(bookBaseURL, book_category, nytApiKey) {
+      return `${bookBaseURL}${this.book_category}.json?api-key=${nytApiKey}`;
+    },
+    getFictionBookList(key) {
+      let bookApiURL = this.buildBookURL(
+        bookBaseURL,
+        this.book_category,
+        nytApiKey
+      );
+
+      axios
+        .get(bookApiURL)
         .then(response => {
-          this.apiDecks.results = response.data.results;
-          console.log("results:", this.apiDecks.results);
-          this.filterBooks(this.apiDecks.results);
-          this.hiddenBooks = false;
+          this.results = response.data.results;
+          this.hiddenFictionBooks = false;
+          this.filterFictionBooks(this.results);
         })
         .catch(error => {
           console.log(error);
         });
     },
-    filterBooks() {
-      this.apiDecks.results.books.map(book => {
+    filterFictionBooks() {
+      this.results.books.map(book => {
         let bookTitle = book.title;
         let bookAuthor = book.author;
         let bookDescription = book.description;
         let bookCover = book.book_image;
-    
-        this.apiDecks.bookDeck.push({
+        let bookURL = book.amazon_product_url;
+
+        this.apiDecks.fictionDeck.push({
           title: bookTitle,
-          description: bookDescription;
+          description: bookDescription,
           card_image: bookCover,
+          url: bookURL
         });
-        this.apiDecks.results = [];
-        return this.apiDecks.bookDeck;
+        this.results = [];
+        return this.apiDecks.fictionDeck;
       });
     },
+    getNonFictionBookList(key) {
+      let bookApiURL = this.buildBookURL(
+        bookBaseURL,
+        this.book_category,
+        nytApiKey
+      );
+
+      axios
+        .get(bookApiURL)
+        .then(response => {
+          this.results = response.data.results;
+          this.hiddenNonFictionBooks = false;
+          this.filterNonFictionBooks(this.results);
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    },
+    filterNonFictionBooks() {
+      this.results.books.map(book => {
+        let bookTitle = book.title;
+        let bookAuthor = book.author;
+        let bookDescription = book.description;
+        let bookCover = book.book_image;
+        let bookURL = book.amazon_product_url;
+
+        this.apiDecks.nonFictionDeck.push({
+          title: bookTitle,
+          description: bookDescription,
+          card_image: bookCover,
+          url: bookURL
+        });
+        this.results = [];
+        return this.apiDecks.nonFictionDeck;
+      });
+    }
   }
 };
 </script>
