f06f348588817a1060b8926a6f6f3c5f17eea1a7
Kyle Heidelberger <kyle.heidelberger@gmail.com>
2019-08-11 20:50:03 -0400

    more style changes

--- src/components/Decks.vue
+++ src/components/Decks.vue
@@ -1,5 +1,5 @@
 <template>
-  <section>
+  <section class="deck-vue">
     <h1 class="page-title">What can we help you decide?</h1>
     <main class="deck" role="main">
       <div v-if="!hiddenSearch" class="searchBar">
