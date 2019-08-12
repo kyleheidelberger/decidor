515dd9fd3a15c815c69be5b229a9f5384a6ac64a
Kyle Heidelberger <kyle.heidelberger@gmail.com>
2019-08-11 21:34:44 -0400

    remove names

--- decider/urls.py
+++ decider/urls.py
@@ -14,7 +14,6 @@ urlpatterns = [
   path('admin/', admin.site.urls),
   path('', views.decks, name='decks'),
   path('index/', views.index, name='info'),
-  path('api', views.names),
   path('decks/', views.decks, name='decks'),
   path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
   path('accounts/', include('allauth.urls')),
