f1b4f70d59b5a4ab5f9869d38426c8573ece4dd9
Kyle Heidelberger <kyle.heidelberger@gmail.com>
2019-08-13 12:41:24 -0400

    add images to how to page

--- src/components/Info.vue
+++ src/components/Info.vue
@@ -1,14 +1,16 @@
 <template>
   <section>
     <h1 class="info-title">Decisions made easy.</h1>
+    <!-- <div class="screenshots-div">
+     <img class="screenshot" id="deck-screenshot" src="//decidor.s3.us-east-2.amazonaws.com/Decks_screen.png" /></div> -->
     <h2 class="what-is">What is decidOR?</h2>
     <p class="whatis-text">
       Can't decide what to do, where to eat, or what to watch?
       decidOR provides a guided experience that enables users to easily make a variety of common decisions when faced with an overwhelming amount of choices. Our application simplifies this process by presenting two options at a time, in order for the user to reach an ultimate decision.
     </p>
     <div class="screenshots-div">
-    <img class="screenshot" id="deck-screenshot" src="//decidor.s3.us-east-2.amazonaws.com/Decks_screen.png" />
     <img class="screenshot" id="choices-screenshot" src="//decidor.s3.us-east-2.amazonaws.com/choices_screen.png" />
+    <img class="screenshot" id="final-screenshot" src="//decidor.s3.us-east-2.amazonaws.com/final_choice.png" />
     <div>
   
     <h3 class="what-is">How to Use decidOR</h3>
